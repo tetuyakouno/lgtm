caa18ff772ca1dde7335520bdeda2dde35785716
tetuyakouno <tetuyakouno@gmail.com>
2020-05-19 14:00:39 +0000

    create a frame of the project"

---
+++ lgtm_exe/core.py
@@ -0,0 +1,11 @@
+import click
+
+@click.command()
+def cli():
+    """A tool of creation LGTM picture"""
+    lgtm()
+    click.echo('lgtm') #for check
+
+def lgtm():
+    # coding logic here.
+    pass
---
+++ main.py
@@ -0,0 +1,4 @@
+from lgtm_exe import core
+
+if __name__ == '__main__':
+    core.cli()
